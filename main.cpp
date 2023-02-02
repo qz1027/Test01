71b6d2003233d69b8294fc71d52dff2d10cd13cc
qz1027 <3315057006@qq.com>
2023-02-02 17:43:30 +0800

    新功能已开发完毕！

--- main.cpp
+++ main.cpp
@@ -11,6 +11,7 @@ int main(){
     cout << "在dev分支下开发新功能，开发到20%！" << endl;
     cout << "在dev分支下开发新功能到50%！" << endl;
     cout << "在家dev分支下开发新功能到80%" << endl;
+    cout << "在公司dev分支下开发新功能完毕！" << endl;
 
     return 0;
 }
